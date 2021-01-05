db960a501a0a90fba77f0b9535f26c7ac7f88c07
andreanreza <rezaandreangithub@gmail.com>
2021-01-05 15:21:21 +0700

    menambahkan fitur baru

---
+++ application/views/poli/dashboard-poliumum.php
@@ -0,0 +1,60 @@
+<div class="content-body">
+
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="card gradient-1">
+                    <div class="card-body">
+                        <h3 class="card-title text-white">sudah diperiksa</h3>
+                        <div class="d-inline-block">
+                            <h2 class="text-white"><?= $terperiksa['jumlah']; ?></h2>
+                            <p class="text-white mb-0"><?= date('d F'); ?></p>
+                            <p class="text-white mb-0"><?= date('Y'); ?></p>
+                        </div>
+                        <span class="float-right display-5 opacity-5"><i class="fa fa-shopping-cart"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card gradient-2">
+                    <div class="card-body">
+                        <h3 class="card-title text-white">belum di periksa</h3>
+                        <div class="d-inline-block">
+                            <h2 class="text-white"><?= $belumperiksa['jumlah']; ?></h2>
+                            <p class="text-white mb-0"><?= date('d F'); ?></p>
+                            <p class="text-white mb-0"><?= date('Y'); ?></p>
+                        </div>
+                        <span class="float-right display-5 opacity-5"><i class="fa fa-money"></i></span>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-lg-3 col-sm-6">
+                <div class="card gradient-3">
+                    <div class="card-body">
+                        <h3 class="card-title text-white">New Customers</h3>
+                        <div class="d-inline-block">
+                            <h2 class="text-white">4565</h2>
+                            <p class="text-white mb-0">Jan - March 2019</p>
+                        </div>
+                        <span class="float-right display-5 opacity-5"><i class="fa fa-users"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card gradient-4">
+                    <div class="card-body">
+                        <h3 class="card-title text-white">Customer Satisfaction</h3>
+                        <div class="d-inline-block">
+                            <h2 class="text-white">99%</h2>
+                            <p class="text-white mb-0">Jan - March 2019</p>
+                        </div>
+                        <span class="float-right display-5 opacity-5"><i class="fa fa-heart"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+
+        </div>
+    </div>
+
+</div>
---
+++ application/views/poli/poliumum-sudahperiksa.php
@@ -0,0 +1,76 @@
+<div class="content-body">
+    <div class="row page-titles mx-0">
+        <div class="col p-md-0">
+            <h4><small><?= $judul; ?></small></h4>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <div class="col-md-6 ml-2">
+                            <?= $this->session->flashdata('pesan'); ?>
+                        </div>
+
+                        <div class="table-responsive ">
+                            <table class="table table-striped table-bordered zero-configuration">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>No Rm</th>
+                                        <th>Jenis Kelamin</th>
+                                        <th>Keluhan</th>
+                                        <th>tgl rekmed</th>
+                                        <th>obat</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $no = 1;
+                                    foreach ($rekmed as $rm) : ?>
+                                        <tr>
+                                            <td><?= $no++; ?></td>
+                                            <td><?= $rm->nama; ?></td>
+                                            <td><?= $rm->no_rm; ?></td>
+
+                                            <td><?= $rm->jenis_kelamin; ?></td>
+                                            <td><?= $rm->keluhan; ?></td>
+                                            <td><?= date('d F Y', $rm->tgl_rekam); ?></td>
+                                            <td>
+                                                <?php
+                                                $id_rm = $rm->id;
+                                                $this->db->select('tbl_rm_obat.*, tbl_rekam.*, tbl_obat.nama_obat');
+                                                $this->db->from('tbl_rm_obat');
+                                                $this->db->join('tbl_rekam', 'tbl_rm_obat.id_rm = tbl_rekam.id');
+                                                $this->db->join('tbl_obat', 'tbl_rm_obat.id_obat = tbl_obat.id');
+                                                $this->db->where('tbl_rm_obat.id_rm', $id_rm);
+                                                $query = $this->db->get()->result();
+                                                ?>
+                                                <?php foreach ($query as $ro) : ?>
+                                                    <?= $ro->nama_obat; ?><br>
+                                                <?php endforeach; ?>
+                                            </td>
+                                            <td>
+                                                <a href="<?= base_url('poliumum/poliumum/cetakpdf/') . $rm->id; ?>" class="btn btn-success"><i class="fas fa-file-pdf mr-2"></i>Cetak</a>
+                                                <a href="<?= base_url('poliumum/poliumum/hapusrekmed/') . $rm->id; ?>" class="btn btn-danger" onclick="return confirm('yakin ?'); ">hapus</a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- #/ container -->
+</div>
+
+
+</div>
